528ca451d00a31a87f12dfc5b223fa96d8fdcd35
Mina <minahilqasim42@gmail.com>
2026-03-20 18:39:07 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,43 @@
+const CACHE_NAME = "eid-vault-v1";
+
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./Favicon.png"
+];
+
+// Install (cache files)
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Activate (cleanup old cache)
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Fetch (serve cached files)
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
+});
